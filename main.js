cedd8339f39a2a581d6c8dac12e4d2d6630cbddc
Jethro Estangki <estangkij@gmail.com>
2020-04-10 19:15:08 +0800

    Make buttons show input on screen on click

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const NUMBERS = document.querySelectorAll("button");
+const IO_PANEL = document.querySelector("#io-panel");
+const HISTORY_PANEL = document.querySelector("#history-panel");
+
+NUMBERS.forEach((number) => {
+  number.addEventListener("click", function (e) {
+    IO_PANEL.textContent += e.target.textContent;
+  });
+});
 
